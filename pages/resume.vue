69b47226d931307985020c9d0a1a5225b1a40706
kilalo <killian.challeau@hotmail.fr>
2022-09-27 21:17:58 +0200

    update projects page + update resume pdf + update skills page text + fix pdf ico

--- pages/resume.vue
+++ pages/resume.vue
@@ -1,65 +1,37 @@
 <template>
-	<v-row
-		class="h100">
-		<v-col
-			cols="12" md="6"
-			class="ma">
-			<v-btn
-				text
-				class="d-block big-btn my-10 ma"
-				:ripple="false">
-				<a
-					href="/CV_KC_2020.pdf"
-					target="_blank">
-					<v-icon
-						class="d-block big-ico ma-2">
-						mdi-file-pdf-outline
-					</v-icon>
-					<span
-						class="fzxs">
-						Télécharger la version PDF
-					</span>
-				</a>
-				</v-btn>
-		</v-col>
-		<v-col
-			cols="12" md="6"
-			class="ma">
-			<v-btn
-				text
-				class="d-block big-btn my-10 ma"
-				:ripple="false">
-				<a 
-					href="https://resume.kilalo.io"
-					target="_blank">
-					<v-icon
-						class="big-ico ma-2">
-						mdi-web
-					</v-icon>
-					<span
-						class="fzxs">
-						Accéder à la version en ligne
-					</span>
-				</a>
-			</v-btn>
-		</v-col>
-	</v-row>
+  <v-row class="h100">
+    <v-col cols="12" md="6" class="ma">
+      <v-btn text class="d-block big-btn my-10 ma" :ripple="false">
+        <a href="/CV_KC_2022.pdf" target="_blank">
+          <v-icon class="d-block big-ico ma-2"> mdi-file-pdf-box</v-icon>
+          <span class="fzxs"> Télécharger la version PDF </span>
+        </a>
+      </v-btn>
+    </v-col>
+    <v-col cols="12" md="6" class="ma">
+      <v-btn text class="d-block big-btn my-10 ma" :ripple="false">
+        <a href="https://resume.kilalo.io" target="_blank">
+          <v-icon class="big-ico ma-2"> mdi-web </v-icon>
+          <span class="fzxs"> Accéder à la version en ligne </span>
+        </a>
+      </v-btn>
+    </v-col>
+  </v-row>
 </template>
 <script>
-
 export default {
-	head: {
-		title: "CV",
-		meta: [
+  head: {
+    title: "CV",
+    meta: [
       {
-        hid: 'description',
-        name: 'description',
-        content: "Consultez mon CV en ligne ou au format PDF."
-      }
-		],
-	},
-	data () {
-		return {}
-	},
-}
+        hid: "description",
+        name: "description",
+        content: "Consultez mon CV en ligne ou au format PDF.",
+      },
+    ],
+  },
+  data() {
+    return {};
+  },
+};
 </script>
